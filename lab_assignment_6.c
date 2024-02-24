08a3b80234003849600a3c78e63b37bfab71b975
RYAN SCHLOSBON <rjs1214@live.com>
2024-02-23 22:27:54 -0500

    updated with comments and new base case

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Ryan Schlosbon - COP 3502C - Lab Assignment 6 - Recursion Search
 int search(int numbers[], int low, int high, int value) 
 {
+	//Base case, checks if low is greater than high in which case it fails
 	if(low > high)
 		return -1;
+	//find the mid point
 	int mid = low + (high-low)/2;
+	//if numbers[mid] == value, return mid as it is equal to the vaue we're searching for (base case)
 	if(numbers[mid] == value){
 		return mid;
 	}
+	//if numbers[mid] is less than the value being searched,. search in the left side of the array by making
+	//mid the low marker and add one to it making the search size smaller
 	else if(numbers[mid]<value){
 		return search(numbers, mid+1, high, value);
 	}
-	else
+	//if numbers[mid] is greater than the value being searched, search in the right side of the array by 
+	//making mid the high marker and subtracting one by it, making the search size smaller
+	else if(numbers[mid]>value)
 		return search(numbers,low,mid-1,value);
-	
-	return -1;
+	//covers low == high case
+	else
+		return mid;
+	return -1; //returns -1 if the function fails
 }
 
 void printArray(int numbers[], int sz)
